5b30466fbc826404f0faf9838a53865319a58375
Michael Davidsaver <mdavidsaver@gmail.com>
2020-12-14 12:43:57 -0800

    example add_dll_directory
    needed on windows for py>=3.8

--- example/src/dsodemo/cli.py
+++ example/src/dsodemo/cli.py
@@ -5,9 +5,12 @@ import sys, os
 
 def fixpath():
     path = os.environ.get('PATH', '').split(os.pathsep)
-    moddir = os.path.dirname(__file__)
-    path.append(os.path.join(moddir, 'lib'))
+    libdir = os.path.join(os.path.dirname(__file__), 'lib')
+    path.append(libdir)
     os.environ['PATH'] = os.pathsep.join(path)
+
+    if hasattr(os, 'add_dll_directory'):
+        os.add_dll_directory(libdir)
 
 if sys.platform == "win32":
     fixpath()
